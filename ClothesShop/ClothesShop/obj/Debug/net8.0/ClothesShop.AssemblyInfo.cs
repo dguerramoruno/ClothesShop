07e2fb7fef6be7679f4706b211d7e10fc067250d
dguerramoruno <dguerramoruno@gmail.com>
2025-02-06 00:52:03 +0100

    Readme Updated

--- ClothesShop/ClothesShop/obj/Debug/net8.0/ClothesShop.AssemblyInfo.cs
+++ ClothesShop/ClothesShop/obj/Debug/net8.0/ClothesShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClothesShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6923065f7dbd4ec97e3246f6e908663577632fd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f6ad3cb031f9071afbe254ba72ae0af82e58556")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClothesShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClothesShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
